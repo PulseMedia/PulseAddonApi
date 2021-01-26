291bb9579285ddaccaf327fe68e04de83c49483b
Pulse-Git <47848626+Pulse-Git@users.noreply.github.com>
2021-01-26 18:25:23 +0100

    Added Native Attribute

---
+++ Native.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace PulseNativeBridge
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class Native : Attribute
+    {
+        public string Family;
+
+        public Native(string family)
+        {
+            this.Family = family;
+        }
+    }
+
+    public class NativeMethod
+    {
+        public string Name;
+        public string Family;
+        public MethodInfo Info;
+        public NativeMethodParameter[] Params;
+        public bool HasParams;
+    }
+
+    public class NativeMethodParameter
+    {
+        public Type Type;
+        public bool Optional;
+        public object Default;
+    }
+}
